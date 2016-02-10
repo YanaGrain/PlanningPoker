f3b69413e22325d8e31a34f7600e46b9dfd3dec8
YanaGrain <anskri@mail.ru>
2016-02-10 16:00:26 +0300

    all_works

---
+++ PlanningPoker/PlanningPoker/app/app.js
@@ -0,0 +1,41 @@
+﻿var app = angular.module('PokerApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar']);
+
+app.config(function ($routeProvider) {
+$routeProvider
+    .when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    })
+
+    .when("/login", {
+        controller: "loginController",
+        templateUrl: "/app/views/login.html"
+    })
+    .when("/about", {
+        controller: "aboutController",
+        templateUrl: "/app/views/about.html"
+    })
+    .when("/contact", {
+        controller: "contactController",
+        templateUrl: "/app/views/contact.html"
+    })
+    .when("/dashboard", {
+        controller: "dashboardController",
+        templateUrl: "/app/views/dashboard.html"
+    })
+
+   .when("/signup", {
+        controller: "signupController",
+        templateUrl: "/app/views/signup.html"
+   })
+
+    .otherwise({ redirectTo: "/home" });
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptorService');
+});
---
+++ PlanningPoker/PlanningPoker/app/controllers/dashboardController.js
@@ -0,0 +1,8 @@
+﻿app.controller('dashboardController', function ($scope) {
+    $scope.message = 'This is your dashboard';
+    $scope.data = { visible: false }
+    $scope.showRoom = function () {
+        $scope.data = { visible: true }
+    }
+
+});
