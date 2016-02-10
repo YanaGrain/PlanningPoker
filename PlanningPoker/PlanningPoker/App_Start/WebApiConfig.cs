32152eff7e21ef7d55b87ccb9fa74d5034c83993
YanaGrain <anskri@mail.ru>
2016-02-10 12:56:11 +0300

    firrst_part_completed

--- PlanningPoker/PlanningPoker/App_Start/WebApiConfig.cs
+++ PlanningPoker/PlanningPoker/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace PlanningPoker
 {
@@ -9,9 +11,8 @@ namespace PlanningPoker
     {
         public static void Register(HttpConfiguration config)
         {
-            // Конфигурация и службы веб-API
 
-            // Маршруты веб-API
+            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
@@ -19,6 +20,9 @@ namespace PlanningPoker
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
--- PlanningPoker/PlanningPoker/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlanningPoker.WebApiApplication" Language="C#" %>
