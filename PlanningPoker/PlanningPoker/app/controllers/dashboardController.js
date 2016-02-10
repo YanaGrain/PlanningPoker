4483e62bdb76785bd58ceb4fc81179b7450dd3e8
YanaGrain <anskri@mail.ru>
2016-02-10 15:05:08 +0300

    some

---
+++ PlanningPoker/PlanningPoker/app/controllers/dashboardController.js
@@ -0,0 +1,7 @@
+﻿pokerApp.controller('dashboardController', authService, function ($scope, authService) {
+    $scope.message = 'This is your dashboard';
+    $scope.data = { visible: false }
+    $scope.showRoom = function () {
+        $scope.data = { visible: true }
+    }
+});
