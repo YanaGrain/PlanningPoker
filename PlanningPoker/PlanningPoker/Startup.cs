7b569c790b195a365fd68dc674b4123820fcb275
YanaGrain <anskri@mail.ru>
2016-02-10 11:58:07 +0300

    initial

---
+++ PlanningPoker/PlanningPoker/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(PlanningPoker.Startup))]
+
+namespace PlanningPoker
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Дополнительные сведения о настройке приложения см. по адресу: http://go.microsoft.com/fwlink/?LinkID=316888
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
