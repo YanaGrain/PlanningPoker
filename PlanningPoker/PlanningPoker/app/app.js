11f88ae6c43dcb86c0ea8e60d7b3f9995ad50a96
YanaGrain <anskri@mail.ru>
2016-02-11 12:48:50 +0300

    all is well

--- PlanningPoker/PlanningPoker/app/app.js
+++ PlanningPoker/PlanningPoker/app/app.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('PokerApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar']);
+﻿var app = angular.module('PokerApp', ['ngRoute', 'LocalStorageModule']);
 
 app.config(function ($routeProvider) {
 $routeProvider
