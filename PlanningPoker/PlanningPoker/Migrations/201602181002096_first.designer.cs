f462136506a1d335497113e8e916bd2b70da249d
YanaGrain <anskri@mail.ru>
2016-02-18 13:25:52 +0300

    new_migrations

--- PlanningPoker/PlanningPoker/Migrations/201602181002096_first.designer.cs
+++ PlanningPoker/PlanningPoker/Migrations/201602181002096_first.designer.cs
@@ -7,13 +7,13 @@ namespace PlanningPoker.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class choicesnew : IMigrationMetadata
+    public sealed partial class first : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(choicesnew));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(first));
         
         string IMigrationMetadata.Id
         {
-            get { return "201602131115008_choices new"; }
+            get { return "201602181002096_first"; }
         }
         
         string IMigrationMetadata.Source
